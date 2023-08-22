1b4f5202355e87219d93615660931b95ae5681b5
anismhaddouche <anisahdd@gmail.com>
2023-08-22 16:16:09 +0200

    docker fixed

--- deployement/test.py
+++ deployement/test.py
@@ -1,13 +1,10 @@
 import requests 
 
 
-
-
-
 data_to_predict =   {
     "commune": 'Hydra',
     "location_duree": 6,
-    "superficie": 250,
+    "superficie": 150,
     "pieces": 8,
     "etages": 1,
     "category": 'Appartement'
